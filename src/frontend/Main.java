c432d7b20f27383c5a31f9832a13a33875280128
Catarina <catisousalima1999@gmail.com>
2023-02-03 09:36:33 +0000

    front end

---
+++ src/frontend/Main.java
@@ -0,0 +1,15 @@
+package src.frontend;
+
+import src.backend.Greeter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		greeter.setEnglish();
+		System.out.println(greeter.greet());
+		greeter.setGerman();
+		System.out.println(greeter.greet());
+	}
+
+}
