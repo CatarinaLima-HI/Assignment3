b9978a115255e1d74e668a99d3ec2a89efe7eb76
Catarina <catisousalima1999@gmail.com>
2023-02-04 09:33:05 +0000

    a lot of changes, I know

---
+++ out/production/Assignment3/compile.cmd
@@ -0,0 +1,4 @@
+javac Main.java
+javac -d bin src/Main.java src/Greeter.java src/Language.java
+javac -d bin src/frontend/Main.java src/backend/Greeter.java src/backend/Language.java
+javac frontend/Main.java backend/Language.java backend/Greeter.java
---
+++ out/production/Assignment3/run.cmd
@@ -0,0 +1,3 @@
+java Main
+java Language (this one is auxiliary to the main method so it does not run)
+java Greeter (this does not have a main method therefore it does not run)
